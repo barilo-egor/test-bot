9b1296ee2ec05155d1c5a97a51fc0f4c5224e9db
gleb <glebpaukov@yamdex.ru>
2023-06-21 22:54:41 +0300

    Регистрация пользователя

--- src/main/java/tgb/test/testbot/bot/Bot.java
+++ src/main/java/tgb/test/testbot/bot/Bot.java
@@ -6,6 +6,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+
 @Service
 public class Bot extends TelegramLongPollingBot {
 
@@ -23,32 +25,63 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         String text = update.getMessage().getText();
         Long chatId = update.getMessage().getChatId();
-        String answerText = run(text);
-        sendMessage(chatId, answerText);
+        String answer = run(text, chatId);
+        sendMessage(chatId, answer);
     }
 
-    public String name;
-
-    public String city;
-
     public int step = 0;
+    ArrayList<User> userList = new ArrayList<>();
 
-    public String run(String inputText) {
+    public String run(String inputText, Long chatId) {
         String answer = null;
 
         if (inputText.equals("/start")) {
-            answer = "Введите свое имя.";
-        } else if (step == 0) {
-            name = inputText;
-            step++;
-            answer = "Введите свой город.";
-        } else if (step == 1) {
-            city = inputText;
-            answer = "Ваше имя: " + name + "\nВаш город:" + city;
+            answer = "Для регистрации напишите /createuser \nДля проверки логина напишите /isexist \nДля проверки userID напишите /getid";
+            step = 1;
+        } else if (!inputText.equals("/createuser") && !inputText.equals("/isexist") && !inputText.equals("/getid") && step == 1) {
+            answer = "Неизвестная команда";
+            return answer;
+        } else if (inputText.equals("/createuser") && step == 1) {
+            answer = "Введите логин";
+            step = 2;
+        } else if (step == 2) {
+            User user = new User(inputText, chatId);
+            userList.add(user);
+            answer = "Вы зарегистрированы";
+            step = 0;
         }
-
-
-        if (answer == null) answer = "Не понимаю";
+        if (inputText.equals("/isexist") && step == 1) {
+            answer = "Введите логин для проверки";
+            step = 3;
+        } else if (step == 3) {
+            for (User user : userList) {
+                if (inputText.equals(user.login)) {
+                    answer = "Пользователь зареган";
+                    step = 0;
+                    break;
+                }
+            }
+            if (answer == null) {
+                answer = "Пользователь не зареган";
+                step = 0;
+            }
+        }
+        if (inputText.equals("/getid") && step == 1) {
+            answer = "Введите логин для проверки userID";
+            step = 4;
+        } else if (step == 4) {
+            for (User user : userList) {
+                if (inputText.equals(user.login)) {
+                    answer = String.valueOf(chatId);
+                    break;
+                }
+            }
+            if (answer == null) {
+                answer = "Ползователь с таким логином не зареган";
+                step = 0;
+            }
+        }
+        if (answer == null) answer = "Для запуска бота, пропишите /start";
         return answer;
     }
 
@@ -63,3 +96,4 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 }
+
---
+++ src/main/java/tgb/test/testbot/bot/User.java
@@ -0,0 +1,11 @@
+package tgb.test.testbot.bot;
+
+public class User {
+    String login;
+    long id;
+
+    public User(String login,long id) {
+        this.login = login;
+        this.id = id;
+    }
+}
