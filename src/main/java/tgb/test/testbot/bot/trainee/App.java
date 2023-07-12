f29cd4fde59d5d11453c5bbc5e510b4b1be29974
barilo-egor <EgorBarilo37552109>
2023-07-12 23:57:49 +0300

    task 00

---
+++ src/main/java/tgb/test/testbot/bot/trainee/App.java
@@ -0,0 +1,11 @@
+package tgb.test.testbot.bot.trainee;
+
+import tgb.test.testbot.bot.trainee.tasks.Task00;
+
+public class App {
+
+    public static void main(String[] args) {
+        Task00 task00 = new Task00();
+        task00.run();
+    }
+}
---
+++ src/main/java/tgb/test/testbot/bot/trainee/tasks/Task00.java
@@ -0,0 +1,11 @@
+package tgb.test.testbot.bot.trainee.tasks;
+
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+public class Task00 {
+
+    public void run() {
+        for (int i = 0; i < 11; i++) {
+            if (i % 2 == 0) System.out.println(i);
+        }
+    }
+}
