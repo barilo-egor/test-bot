d5e718233fb1dbc8a4c0fefa3d884d15a05a39f4
gleb <glebpaukov@yamdex.ru>
2023-07-14 00:03:24 +0300

    Task00

--- src/main/java/tgb/test/testbot/bot/trainee/tasks/Task00.java
+++ src/main/java/tgb/test/testbot/bot/trainee/tasks/Task00.java
@@ -1,11 +1,22 @@
 package tgb.test.testbot.bot.trainee.tasks;
 
+import java.util.Scanner;
+
 // https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
 public class Task00 {
 
     public void run() {
-        for (int i = 0; i < 11; i++) {
-            if (i % 2 == 0) System.out.println(i);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите кол-во обезьян");
+        int number = in.nextInt();
+        System.out.println("Подсчет обезбян: ");
+        int[] monkey = new int[number];
+        for (int i = 0; i < monkey.length; i++) {
+            monkey[i] = i + 1;
+            System.out.print(monkey[i]);
+            if (i != monkey.length - 1) {
+                System.out.print(",");
+            }
         }
     }
 }
